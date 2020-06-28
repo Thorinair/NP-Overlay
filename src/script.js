f67825d906ac16ae1e2799d40c55d30573a2ae3b
Thorinair <thorinair@yahoo.com>
2020-06-28 15:35:52 +0200

    Added fade.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+var element = 1;
+
 function refresh() {
     xhr = new XMLHttpRequest();
     var url = "http://luna.thorinair.net:" + port + "/?action=np&key=" + key;
@@ -5,7 +7,25 @@ function refresh() {
     xhr.onreadystatechange = function () { 
         if (xhr.readyState == 4 && xhr.status == 200) {
             var response = JSON.parse(xhr.responseText);
-            document.getElementById("np").innerHTML = response.nowplaying;
+
+            var np = document.getElementById("np" + element);
+            var np_data = document.getElementById("np" + element + "_data");
+
+            if (np_data.innerHTML != response.nowplaying) {
+
+                if (element == 1)
+                    element = 2;
+                else
+                    element = 1;
+
+                var npx = document.getElementById("np" + element);
+                var npx_data = document.getElementById("np" + element + "_data");
+
+                npx_data.innerHTML = response.nowplaying;
+
+                np.classList.toggle('fade');
+                npx.classList.toggle('fade');
+            }
         }
     }
     xhr.onerror = function () {
